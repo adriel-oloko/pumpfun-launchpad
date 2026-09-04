d56a60310f55d69e205cfd9cb4943790425a5e3b
Adriel Oloko <adrieloloko6@gmail.com>
2026-09-04 10:11:45 +0100

    asdfghj

--- components/launch-panel.tsx
+++ components/launch-panel.tsx
@@ -629,7 +629,7 @@ export function LaunchPanel({
               </p>
             )}
           </div>
-          <div className="grid md:grid-cols-2 lg:grid-cols-3">
+          <div className="min-w-full grid md:grid-cols-2 lg:grid-cols-3">
           {selectedWallets.map((w) => (
             <Field
               key={w.address}
